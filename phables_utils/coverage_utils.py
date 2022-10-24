e250e4c685fe421bae53e1d1f3bccc50d064fae0
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2022-10-24 17:17:03 +1030

    Add output and coverage utils

---
+++ phables_utils/coverage_utils.py
@@ -0,0 +1,66 @@
+from collections import defaultdict
+import pysam
+import glob
+
+def get_edge_coverage(coverage):
+
+    contig_coverages = {}
+
+    with open(coverage, "r") as myfile:
+        for line in myfile.readlines():
+
+            if not line.startswith("Contig"):
+                strings = line.strip().split()
+
+                contig_name = strings[0].replace("contig", "edge")
+
+                coverage_sum = sum([float(x) for x in strings[1:]])
+
+                contig_coverages[contig_name] = coverage_sum
+
+    return contig_coverages
+
+
+def read_pair_generator(bam, region_string=None):
+    """
+    Generate read pairs in a BAM file or within a region string.
+    Reads are added to read_dict until a pair is found.
+    """
+    read_dict = defaultdict(lambda: [None, None])
+    
+    for read in bam.fetch(region=region_string):
+        if read.is_secondary or read.is_supplementary:
+            continue
+        qname = read.query_name
+        if qname not in read_dict:
+            if read.is_read1:
+                read_dict[qname][0] = read
+            else:
+                read_dict[qname][1] = read
+        else:
+            if read.is_read1:
+                yield read, read_dict[qname][1]
+            else:
+                yield read_dict[qname][0], read
+            del read_dict[qname]
+
+    return read_dict
+
+
+def get_junction_pe_coverage(bam_path):
+
+    bam_files = glob.glob(bam_path+"/*.bam")
+
+    link_counts = defaultdict(int)
+    
+    for bam_file in bam_files:
+
+        bam = pysam.AlignmentFile(bam_file, 'rb')
+
+        read_pairs = read_pair_generator(bam)
+
+        for read1, read2 in read_pairs:
+            if read1.reference_name != read2.reference_name:
+                link_counts[(read1.reference_name, read2.reference_name)] += 1
+
+    return link_counts
